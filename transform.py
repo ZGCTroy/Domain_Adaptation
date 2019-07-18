ac58ef507549689e7c6bf1fe2189baaead7fb2af
郑光聪 <zhengguangcong@ZGCTroy.local>
2019-07-18 13:25:00 +0800

    bug fix on CeterCrop

--- transform.py
+++ transform.py
@@ -35,7 +35,7 @@ def transform_for_Office(resize_size, crop_size):
         ]),
         'test': transforms.Compose([
             transforms.Resize(resize_size),
-            transforms.CenterCrop(crop_size)
+            transforms.CenterCrop(crop_size),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
